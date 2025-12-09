21764c2d58cf2b80c2a90669a89c3d372e6860ed
Eduardo Ferro <eduardo.ferro@clarity.ai>
2025-12-09 19:36:26 +0100

    Remove mamba references from setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,15 @@
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
 
 long_description = open('README.md').read()
 
-
-class MambaTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import sys
-        import mamba.cli
-        sys.argv = ['mamba']
-        mamba.cli.main()
-
-setup(name='cmdweaver',
-      version='0.9.3',
-      author='Eduardo Ferro Aldama',
-      author_email='eduardo.ferro.aldama@gmail.com',
-      description ='Extensible command line processor for "ad hoc" shells creation',
-      long_description=long_description,
-      license='MIT',
-      platforms = 'Linux',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests', 'spec']),
-      cmdclass={'test': MambaTest}
+setup(
+    name='cmdweaver',
+    version='0.9.3',
+    author='Eduardo Ferro Aldama',
+    author_email='eduardo.ferro.aldama@gmail.com',
+    description='Extensible command line processor for "ad hoc" shells creation',
+    long_description=long_description,
+    license='MIT',
+    platforms='Linux',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
 )
